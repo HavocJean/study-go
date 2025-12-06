766209e96abcde44c88b371dafef400f9a241f88
Jean <jean.magic@hotmail.com>
2025-12-05 22:41:47 -0300

    feat: start configuration in main go and create user request and response

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/HavocJean/study-go/internal/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +14,10 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("PORT"))
+	router := gin.Default()
+	routes.InitiRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8082"); err != nil {
+		log.Fatal(err)
+	}
 }
