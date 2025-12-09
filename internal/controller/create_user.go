8b00e65744dad268e498ca4ea8e3f0d127ebe48b
Jean <jean.magic@hotmail.com>
2025-12-08 22:04:54 -0300

    feat: improvements to the create user controller

--- internal/controller/create_user.go
+++ internal/controller/create_user.go
@@ -1,7 +1,38 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/HavocJean/study-go/internal/config/validation"
+	"github.com/HavocJean/study-go/internal/controller/model/request"
+	"github.com/HavocJean/study-go/internal/logger"
+	"github.com/HavocJean/study-go/internal/model"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(int(errRest.Code), errRest)
+		return
+	}
+
+	domain := model.NewUserDomain(
+		userRequest.Email,
+		userRequest.Password,
+		userRequest.Name,
+		userRequest.Age,
+	)
+
+	if err := domain.CreateUser(); err != nil {
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info("User created successfully")
+
+	c.String(http.StatusOK, "")
 }
