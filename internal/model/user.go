3f4f3897bdb8ad1cd128c634a00bfd45258d9dad
Jean <jean.magic@hotmail.com>
2025-12-08 21:26:42 -0300

    feat: create user model and interface

---
+++ internal/model/user.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/HavocJean/study-go/internal/config/rest_error"
+)
+
+func NewUser(email, password, name string, age int8) *User {
+	return &User{
+		email, password, name, age,
+	}
+}
+
+type User struct {
+	Email    string
+	Password string
+	Name     string
+	Age      int8
+}
+
+func (u *User) EncryptPassword() {
+	hash := md5.New()
+	defer hash.Reset()
+	hash.Write([]byte(u.Password))
+	u.Password = hex.EncodeToString(hash.Sum(nil))
+}
+
+type UserInterface interface {
+	CreateUser() *rest_error.RestError
+	UpdateUser(string) *rest_error.RestError
+	FindUser(string) (*User, *rest_error.RestError)
+	DeleteUser(string) *rest_error.RestError
+}
