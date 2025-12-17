d915ba46fd43af7508196ef67a4c87d32d215f8e
Jean <jean.magic@hotmail.com>
2025-12-16 22:59:29 -0300

    refactor: changed the domain names for ease of use and identification, and made minor adjustments to the domains

---
+++ internal/model/user_domain.go
@@ -0,0 +1,33 @@
+package model
+
+type userDomain struct {
+	id       string
+	email    string
+	password string
+	name     string
+	age      int8
+}
+
+func (u *userDomain) GetEmail() string {
+	return u.email
+}
+
+func (u *userDomain) GetPassword() string {
+	return u.password
+}
+
+func (u *userDomain) GetName() string {
+	return u.name
+}
+
+func (u *userDomain) GetAge() int8 {
+	return u.age
+}
+
+func (u *userDomain) GetID() string {
+	return u.id
+}
+
+func (ud *userDomain) SetID(id string) {
+	ud.id = id
+}
