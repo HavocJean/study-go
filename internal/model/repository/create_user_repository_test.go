dcd132a6ad757e697c8f5b6f97d330843a3f463e
Jean <jean.magic@hotmail.com>
2026-01-12 21:18:37 -0300

    feat: start of unit test for use creation

---
+++ internal/model/repository/create_user_repository_test.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"os"
+	"testing"
+
+	"github.com/HavocJean/study-go/internal/model"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+)
+
+func TestUserRepository_CreateUser(t *testing.T) {
+	database_name := "user_database_test"
+	collection_name := "user_collection_test"
+
+	os.Setenv("MONGODB_USER_DB", collection_name)
+	defer os.Clearenv()
+
+	mtestDb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	mtestDb.Run("when_sending_valid_domain_returns_success", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 1},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: true},
+		})
+
+		databaseMock := mt.Client.Database(database_name)
+
+		repo := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain("teste@test.com", "test", "test", 90)
+		userDomain, err := repo.CreateUser(domain)
+
+		_, errId := primitive.ObjectIDFromHex(userDomain.GetID())
+
+		assert.NotNil(t, err)
+		assert.NotNil(t, errId)
+		assert.EqualValues(t, userDomain.GetEmail(), domain.GetEmail())
+		assert.EqualValues(t, userDomain.GetName(), domain.GetName())
+		assert.EqualValues(t, userDomain.GetAge(), domain.GetAge())
+		assert.EqualValues(t, userDomain.GetPassword(), domain.GetPassword())
+
+	})
+
+}
